48a5fa7e45ae0d154f6cc1dae32a09104252ec42
SongJuHo-0410 <sojh0410@naver.com>
2023-09-11 16:20:57 +0900

    update

---
+++ java/chap04/src/Circle2Demo.java
@@ -0,0 +1,13 @@
+class Circle{
+    private double radius;
+
+    public Circle(double r){
+        radius = r;
+    }
+}
+
+public class Circle2Demo{
+    public static void main(String[] args){
+        Circle myCircle = new Circle(10.0);
+    }
+}
---
+++ java/chap04/src/LocalVariableDemo.java
@@ -0,0 +1,12 @@
+public class LocalVariableDemo {
+    public static void main(String[] args){
+        int a = 0;
+        double b;
+
+        int c = 0;
+
+        for(int e = 0; e < 10; e++){
+            System.out.print(e);
+        }
+    }
+}
