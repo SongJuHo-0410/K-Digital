ed55527eabe31d1af8bc6df06d698e68b26bd476
SongJuHo-0410 <sojh0410@naver.com>
2023-09-11 14:55:48 +0900

    update

--- java/chap03/src/Practice2.java
+++ java/chap03/src/Practice2.java
@@ -1,10 +1,10 @@
 public class Practice2 {
-    public static void main(String args[]){
-        for(int i=1;i<=10;i++){
-            for(int j=0;j<i;j++){
+    public static void main(String[] args){
+        for(int i=10; i>0; i--){
+            for(int j=1; j<=i; j++){
                 System.out.print("*");
             }
-            System.out.println("");
+            System.out.println();
         }
     }
 }
