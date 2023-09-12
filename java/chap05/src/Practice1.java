bc7e418d9a8d8954422b678632923992ae35c2f1
SongJuHo-0410 <sojh0410@naver.com>
2023-09-12 11:22:30 +0900

    update

---
+++ java/chap05/src/Practice1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Practice1 {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        System.out.print("문자열 입력:");
+        String s = in.nextLine();
+        //String strReverse;
+
+        for(int i=s.length(); 0<=i; i--){
+            System.out.print(s.charAt(i-1));
+            //strReverse += String.format("%c", str.charAt(i-1));
+        }
+    }
+}
