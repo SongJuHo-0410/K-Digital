fb72cc17f0d53e8f0997c18566277069eabb665b
SongJuHo-0410 <sojh0410@naver.com>
2023-09-15 16:05:37 +0900

    update

---
+++ java/chap07/src/RemoteControllable.java
@@ -0,0 +1,5 @@
+public interface RemoteControllable extends Controllable{
+    void remoteOn();
+
+    void remoteOff();
+}
---
+++ java/chap07/src/TV.java
@@ -0,0 +1,9 @@
+public class TV implements Controllable{
+     public void turnOn(){
+        System.out.println("TV를 켠다.");
+     }
+
+     public void turnOff(){
+        System.out.println("TV를 끈다.");
+     }
+}
